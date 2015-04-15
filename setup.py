83d346a10a06cb806acf6113c921b9a1d62db21a
unknown <D037732@WDFV00355747B.emea.global.corp.sap>
2015-04-15 14:00:13 +0200

    Python 3.4 egg for win64

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-# Copyright 2013 SAP AG.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http: //www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an 
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-# either express or implied. See the License for the specific 
-# language governing permissions and limitations under the License.
 
 import os
 import sys
@@ -20,7 +7,7 @@ from distutils.extension import Extension
 # Note: Historically, the default setup.py did _not_ contain cython requirements.
 #       To build just the extensions inplace, use:
 #       python setup.py build_ext --inplace
-CYTHON_VERSION = '0.21.2'  # fixed to assure conscious change of version.
+CYTHON_VERSION = '0.22'  # fixed to assure conscious change of version.
 try:
     import Cython
 except ImportError:
@@ -30,6 +17,11 @@ except ImportError:
 SAPNWRFC_HOME = os.environ.get('SAPNWRFC_HOME')
 if not SAPNWRFC_HOME:
     sys.exit("Environment variable SAPNWRFC_HOME not set. Please specify this variable with the root directory of the SAP NW RFC Library.")
+# Python sources
+PYTHONSOURCE = os.environ.get('PYTHONSOURCE')
+if not PYTHONSOURCE:
+    sys.exit("Environment variable PYTHONSOURCE not set. Please specify this variable with the root directory of the PYTHONSOURCE Library.")
+
 
 NAME = 'pyrfc'
 here = os.path.abspath(os.path.dirname(__file__))
@@ -41,14 +33,14 @@ def _read(name):
 if sys.platform.startswith('linux'):
     LIBS = ['sapnwrfc', 'sapucum']
     MACROS = [('NDEBUG', None), ('_LARGEFILE_SOURCE', None), ('_FILE_OFFSET_BITS', 64), ('SAPonUNIX', None), ('SAPwithUNICODE', None) , ('SAPwithTHREADS', None), ('SAPonLIN', None)]
-    COMPILE_ARGS = ['-Wall', '-O2', '-fexceptions', '-funsigned-char', '-fno-strict-aliasing', '-Wall', '-Wno-uninitialized', '-Wcast-align', '-fPIC', '-pthread', '-minline-all-stringops', '-I{0}/include'.format(SAPNWRFC_HOME)]
-    LINK_ARGS = ['-L{0}/lib'.format(SAPNWRFC_HOME)]
+    COMPILE_ARGS = ['-Wall', '-O2', '-fexceptions', '-funsigned-char', '-fno-strict-aliasing', '-Wall', '-Wno-uninitialized', '-Wcast-align', '-fPIC', '-pthread', '-minline-all-stringops', '-I{}/include'.format(SAPNWRFC_HOME)]
+    LINK_ARGS = ['-L{}/lib'.format(SAPNWRFC_HOME)]
 elif sys.platform.startswith('win'):
     LIBS = ['sapnwrfc', 'libsapucum']
     MACROS = [('_LARGEFILE_SOURCE', None), ('SAPwithUNICODE', None), ('_CONSOLE', None), ('WIN32', None), ('SAPonNT', None), ('SAP_PLATFORM_MAKENAME', 'ntintel'), ('UNICODE', None), ('_UNICODE', None)]
-    COMPILE_ARGS = ['-I{0}\\include'.format(SAPNWRFC_HOME)]
+    COMPILE_ARGS = ['-I{}\\include'.format(SAPNWRFC_HOME), '-I{}\\Include'.format(PYTHONSOURCE), '-I{}\\Include\\PC'.format(PYTHONSOURCE)]
     #LINK_ARGS = ['-L{}\\lib'.format(SAPNWRFC_HOME)] # JK: Does not work with MS VS8.0 Linker, works with MinGW?
-    LINK_ARGS = ['-LIBPATH:{0}\\lib'.format(SAPNWRFC_HOME)]
+    LINK_ARGS = ['-LIBPATH:{}\\lib'.format(SAPNWRFC_HOME), '-LIBPATH:{}\\PCbuild'.format(PYTHONSOURCE)]
 
 pyrfc = Extension( '%s._%s' % (NAME, NAME)
     , ['%s/_%s.pyx' % (NAME, NAME)]
@@ -69,9 +61,7 @@ setup(name=NAME,
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Cython',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
     keywords='%s sap' % NAME,
