f75550684b2ea94b09cbb2163cb72a8e3c7f3046
bsrdjan <srdjan.boskovic@sap.com>
2018-07-31 14:37:32 +0200

    #72 __version__ added

--- src/pyrfc/__init__.py
+++ src/pyrfc/__init__.py
@@ -21,6 +21,7 @@ from pyrfc._exception import RFCError, RFCLibError,\
 
 from pyrfc._pyrfc import get_nwrfclib_version, Connection, TypeDescription, FunctionDescription, Server
 
+__version__ = "1.9.91"
+
 # TODO: define __all__ variable
 #
-
