f53f3aa4d6693c66260424e0ba4b76d613411e97
bsrdjan <srdjan.boskovic@sap.com>
2019-10-29 21:22:59 +0100

    3.6 added

--- setup.py
+++ setup.py
@@ -149,6 +149,7 @@ setup(
         "Programming Language :: Cython",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
