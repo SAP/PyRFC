d68cc62298d945ebeffe788c1699fe05eca5f4fe
bsrdjan <srdjan.boskovic@sap.com>
2019-11-06 11:02:39 +0100

    Windows compiler settings

--- src/pyrfc/_pyrfc.pyx
+++ src/pyrfc/_pyrfc.pyx
@@ -1299,7 +1299,7 @@ cdef fillVariable(RFCTYPE typ, RFC_FUNCTION_HANDLE container, SAP_UC* cName, val
             fillTable(typeDesc, table, value)
         elif typ == RFCTYPE_BYTE:
             bValue = fillBytes(value)
-            rc = RfcSetBytes(container, cName, bValue, len(value), &errorInfo)
+            rc = RfcSetBytes(container, cName, bValue, int(len(value)), &errorInfo)
             free(bValue)
         elif typ == RFCTYPE_XSTRING:
             bValue = fillBytes(value)
